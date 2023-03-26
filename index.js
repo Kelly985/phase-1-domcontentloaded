c18b5e3688e6e7318c2dd5c18a619631fbb2efa2
+ <+>
2023-03-26 09:59:16 +0300

    completed domloaded lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textElem = document.getElementById('text');
+    textElem.textContent = 'This is really cool!';
+    
+  });
